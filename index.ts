45445de20e0cf3163d99c473d863bec3f18605a3
Rimsha Ansari <159952102+leadership286@users.noreply.github.com>
2024-04-06 00:47:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+console.log(chalk.magenta('Welcome  to Currency Converter'))
+
+const currency :any = {
+    USD :1, //Base Currency
+    EUR : 0.91,
+    GBP:0.76,
+    INR: 74.57,
+    PKR :280
+};
+ let user_answer =  await inquirer.prompt(
+    [
+        {
+         name:"from",
+         message:chalk.blue("Enter From Currency"),
+         type: "list",
+         choices:['USD','EUR','GBP','INR','PKR']
+        },
+        {
+            name:"to",
+            message:chalk.yellow("Enter To Currency"),
+            type: "list",
+            choices:['USD','EUR','GBP','INR','PKR']
+           },
+           {
+            name:"amount",
+            message:chalk.cyan("Kindly Enter Your Amount."),
+            type: "number",
+
+           }
+    ]
+)
+let fromAmount =   currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+
+
